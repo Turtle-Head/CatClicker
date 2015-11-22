692c713a5350c4c0ebd6bae4d002eb533766c3fb
Turtle-Head <djinn.jecture@gmail.com>
2015-11-22 16:27:53 -0600

    Lots of changes
    --updated style.css for new divs and ids
    --changed index.html to show a picture to click on
    --added jquery as a packaged source rather than linking to a CDN
    --changed script.js:
    -removed several functions that were not working
    -rewrote the mousedown function using jquery to get the click and
    increment points

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ Object of Game is to Click on the Cat and Earn Points
 #-------------
 #-Have the Cat move around on the Canvas randomly
 #-If the player clicks on the Cat the points go up
-
+#-------------
+# Current Release
+#-------------
+#-Currently the game checks to see if the cat is clicked on
+#-the image atm doesn't move but clicking on the cat increments the points
--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 //-------------------------
 // global variables go here
-var canvas_x, canvas_y = 0;
-
-//-------------------------
-// functions go here
-
-// initialize the mousedown eventlistener
-function init(){
-  var canvas = document.getElementByID("cvs");
-  cnv.addEventListener("mousedown", doMouseDown, false);
-}
-//update the co-ordinates of the mousedown event
-function doMouseDown(event){
-  canvas_x = event.pageX;
-  canvas_y = event.pageY;
-}
+var points = 0;
+var $pointsElem = $("#point");
+var ticker = document.getElementById("#point");
+// click function
+$("#pic").click(function() {
+  points+=1;
+  console.log(points);
+  $pointsElem.text(points);
+});
