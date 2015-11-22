0c9f159ac3845af1b35dba4e4758d711f88d840c
Turtle-Head <djinn.jecture@gmail.com>
2015-11-22 17:28:52 -0600

    More cats
    --added a second cat, names and points values

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 //-------------------------
 // global variables go here
 var points = 0;
+var points2 = 0;
 var $pointsElem = $("#point");
-var ticker = document.getElementById("#point");
+var $points2Elem = $("#point2");
+
+var name1 = "Nate";
+var name2 = "Jessica";
+$pointsElem.text(name1 + " " + points);
+$points2Elem.text(name2 + " " + points2);
 // click function
 $("#pic").click(function() {
   points+=1;
-  console.log(points);
-  $pointsElem.text(points);
+  console.log("p1: "+points);
+  $pointsElem.text(name1 + " " + points);
+});
+$("#pic2").click(function() {
+  points2+=1;
+  console.log("p2: "+points2);
+  $points2Elem.text(name2 + " " + points2);
 });
