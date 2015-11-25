d1e2382c0a1a7e99c49dde7e81a6ed5049b83ffa
Turtle-Head <djinn.jecture@gmail.com>
2015-11-25 13:45:04 -0600

    changes to reflect current goals
    goals changed to multiple cats
    --switched to json structure for holding elements that populate a list
    of cats to click on

--- js/script.js
+++ js/script.js
@@ -3,20 +3,60 @@
 var points = 0;
 var points2 = 0;
 var $pointsElem = $("#point");
-var $points2Elem = $("#point2");
-
+var HTMLcat = '<div class="flex" id="point">0</div><div id="cat"><img id="pic" class="flex" src="%img%" width="100px" height="100px" alt="%name%"></div>';
+var HTMLlist= '<div class="list-flex">%name%</div>';
 var name1 = "Nate";
 var name2 = "Jessica";
-$pointsElem.text(name1 + " " + points);
+var current = 0;
+var formattedList= "";
+var cats = {
+  "cats":[
+      {
+        "name" : "Nate",
+        "img" : "img/cat.png",
+        "points": 0,
+        "index": 0
+      },
+      {
+        "name": "Jessica",
+        "img": "img/cat.png",
+        "points": 0,
+        "index": 1
+      },
+      {
+        "name": "Joey",
+        "img": "img/cat.png",
+        "points": 0,
+        "index": 2
+      },
+      {
+        "name": "Abby",
+        "img": "img/cat.png",
+        "points": 0,
+        "index": 3
+      },
+      {
+        "name": "Spaz",
+        "img": "img/cat.png",
+        "points": 0,
+        "index": 4
+      }
+    ],
+  display: function (){
+
+    for (var i=0; i < cats.cats.length; i++){
+      formattedList += HTMLlist.replace("%name%",cats.cats[i].name);
+    }
+    $('#catNames').append(formattedList);
+  }
+};
+/*$pointsElem.text(name1 + " " + points);
 $points2Elem.text(name2 + " " + points2);
 // click function
-$("#pic").click(function() {
-  points+=1;
+/*$("#pic").click(function(current) {
+  cats[current].points+=1;
   console.log("p1: "+points);
-  $pointsElem.text(name1 + " " + points);
-});
-$("#pic2").click(function() {
-  points2+=1;
-  console.log("p2: "+points2);
-  $points2Elem.text(name2 + " " + points2);
-});
+  $pointsElem.text(cats[current].name + " " + cats[current].points);
+});*/
+
+cats.display();
