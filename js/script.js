b04867a4bda42c56d92644e50348b68adbdf96c4
Turtle-Head <djinn.jecture@gmail.com>
2015-11-25 20:51:19 -0600

    Optimized CC for 5 cats
    --created click functions for each name div
    --created function to place cat on screen as well as points and name

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@
 var points = 0;
 var points2 = 0;
 var $pointsElem = $("#point");
-var HTMLcat = '<div class="flex" id="point">0</div><div id="cat"><img id="pic" class="flex" src="%img%" width="100px" height="100px" alt="%name%"></div>';
-var HTMLlist= '<div class="list-flex">%name%</div>';
+var $ctx = $("#ctx");
+var HTMLcat = '<div id="name">%nnn%</div><div id="point">%p%</div><div id="cat"><img id="pic" src="%img%" width="100px" height="100px" alt="%name%"></div>';
+var HTMLlist= '<div class="list-flex" id="%index%">%name%</div>';
 var name1 = "Nate";
 var name2 = "Jessica";
 var current = 0;
-var formattedList= "";
+var formattedList='';
 var cats = {
   "cats":[
       {
@@ -45,18 +46,47 @@ var cats = {
   display: function (){
 
     for (var i=0; i < cats.cats.length; i++){
-      formattedList += HTMLlist.replace("%name%",cats.cats[i].name);
+      formattedList += HTMLlist.replace("%name%",cats.cats[i].name).replace("%index%",cats.cats[i].name);
     }
     $('#catNames').append(formattedList);
   }
 };
-/*$pointsElem.text(name1 + " " + points);
-$points2Elem.text(name2 + " " + points2);
-// click function
-/*$("#pic").click(function(current) {
-  cats[current].points+=1;
-  console.log("p1: "+points);
-  $pointsElem.text(cats[current].name + " " + cats[current].points);
-});*/
-
 cats.display();
+//-------------------------
+// Click handlers
+
+// Cat click handler and image changer rolled together
+function setCat(){
+  var formattedPic = HTMLcat.replace("%nnn%",cats.cats[current].name).replace("%p%",cats.cats[current].points).replace("%img%",cats.cats[current].img).replace("%name%",cats.cats[current].name);
+  // Replace image, points and name
+  $("#ctx").replaceWith('<div class="image" id="ctx">' + formattedPic + '</div>');
+  console.log(cats.cats[current].name);
+  // Count clicks for current cat
+  $("#pic").click(function() {
+    cats.cats[current].points+=1;
+    console.log("p"+current+": "+cats.cats[current].points);
+    $("#point").text(cats.cats[current].points);
+  });
+}
+
+// Name div click handlers
+$("#Nate").click(function(){
+  current = 0;
+  setCat();
+});
+$("#Jessica").click(function(){
+  current = 1;
+  setCat();
+});
+$("#Joey").click(function(){
+  current = 2;
+  setCat();
+});
+$("#Abby").click(function(){
+  current = 3;
+  setCat();
+});
+$("#Spaz").click(function(){
+  current = 4;
+  setCat();
+});
